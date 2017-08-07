63d7cb91d14b1b3ac6bb2d0e4754d07a46f6ea97
Owen <owenca@users.noreply.github.com>
2017-08-07 15:22:32 +0000

    Use GridLayout for most of ConfilictWindow.
    Also, change mouse cursor to FOLLOW_LINK for source/target folder path.
    
    Fixes #107

--- sources/ConflictWindow.cpp
+++ sources/ConflictWindow.cpp
@@ -12,6 +12,7 @@
 
 #include <Button.h>
 #include <Catalog.h>
+#include <Cursor.h>
 #include <DateTimeFormat.h>
 #include <Directory.h>
 #include <LayoutBuilder.h>
@@ -21,14 +22,44 @@
 #undef B_TRANSLATION_CONTEXT
 #define B_TRANSLATION_CONTEXT "ConflictWindow"
 
-#define CREATE_TRANSLATED_VIEW(str, var, view) \
-	text = B_TRANSLATE_COMMENT(str ": %" #var "%", \
-		"Don't translate the variable %" #var "%"); \
-	text.ReplaceFirst("%" #var "%", var); \
-	BStringView* view = new BStringView("", text)
-
 static const unsigned kReplace = 'RPLC';
 static const unsigned kSkip = 'SKIP';
+
+
+class FolderView : public BStringView
+{
+	void	MouseMoved(BPoint point, uint32 transit, const BMessage* dragMsg);
+
+public:
+			FolderView(const BString& path);
+};
+
+
+FolderView::FolderView(const BString& path)
+	:
+	BStringView("", path)
+{
+}
+
+
+void
+FolderView::MouseMoved(BPoint point, uint32 transit, const BMessage* dragMsg)
+{
+	switch (transit) {
+		case B_ENTERED_VIEW:
+		{
+			const BCursor cursor(B_CURSOR_ID_FOLLOW_LINK);
+			SetViewCursor(&cursor);
+			break;
+		}
+		case B_EXITED_VIEW:
+		{
+			const BCursor cursor(B_CURSOR_ID_SYSTEM_DEFAULT);
+			SetViewCursor(&cursor);
+			break;
+		}
+	}
+}
 
 
 ConflictWindow::ConflictWindow(const char* file, const char* src,
@@ -36,44 +67,35 @@ ConflictWindow::ConflictWindow(const char* file, const char* src,
 	:
 	BWindow(BRect(0, 0, 0, 0), B_TRANSLATE("Filer: Conflict"),
 		B_FLOATING_WINDOW_LOOK, B_FLOATING_ALL_WINDOW_FEEL,
-		B_NOT_CLOSABLE | B_NOT_ZOOMABLE | B_NOT_RESIZABLE | B_AVOID_FOCUS |
-		B_AUTO_UPDATE_SIZE_LIMITS),
+		B_NOT_CLOSABLE | B_NOT_ZOOMABLE | B_NOT_RESIZABLE | B_AVOID_FOCUS
+			| B_AUTO_UPDATE_SIZE_LIMITS),
 	fDoAll(new BCheckBox("", B_TRANSLATE("Do this for all files"), NULL)),
 	fReplace(false),
 	fSem(create_sem(0, ""))
 {
 	BStringView* info = new BStringView("",
 		"File already exists in target folder.");
-	info->SetAlignment(B_ALIGN_CENTER);
-
-	BString text;
-
-	CREATE_TRANSLATED_VIEW("File name", file, fileName);
-
-	CREATE_TRANSLATED_VIEW("Source folder", src, srcFolder);
-	BStringView* srcInfo = _CreateAttrView(file, src);
-
-	CREATE_TRANSLATED_VIEW("Target folder", dest, destFolder);
-	BStringView* destInfo = _CreateAttrView(file, dest);
-
-	CREATE_TRANSLATED_VIEW("Rule name", desc, ruleDesc);
 
 	BButton* replace = new BButton("", B_TRANSLATE("Replace"),
 		new BMessage(kReplace));
 	BButton* skip = new BButton("", B_TRANSLATE("Skip"), new BMessage(kSkip));
 
-	BLayoutBuilder::Group<>(this, B_VERTICAL, B_USE_DEFAULT_SPACING)
+	BLayoutBuilder::Group<>(this, B_VERTICAL)
 		.SetInsets(B_USE_BIG_INSETS)
-		.AddGroup(B_VERTICAL, 0)
-			.Add(info)
-			.Add(fileName)
-			.Add(srcFolder)
-			.Add(srcInfo)
-			.Add(destFolder)
-			.Add(destInfo)
-			.Add(ruleDesc)
+		.Add(info)
+		.AddGrid(B_USE_HALF_ITEM_SPACING, 0)
+			.Add(_CreateLabelView(B_TRANSLATE("File name")), 0, 0)
+			.Add(new BStringView("", file), 1, 0)
+			.Add(_CreateLabelView(B_TRANSLATE("Source folder")), 0, 1)
+			.Add(new FolderView(src), 1, 1)
+			.Add(_CreateAttrView(file, src), 1, 2)
+			.Add(_CreateLabelView(B_TRANSLATE("Target folder")), 0, 3)
+			.Add(new FolderView(dest), 1, 3)
+			.Add(_CreateAttrView(file, dest), 1, 4)
+			.Add(_CreateLabelView(B_TRANSLATE("Rule name")), 0, 5)
+			.Add(new BStringView("", desc), 1, 5)
 			.End()
-		.AddGroup(B_HORIZONTAL, B_USE_DEFAULT_SPACING)
+		.AddGroup(B_HORIZONTAL)
 			.AddGlue()
 			.Add(skip)
 			.Add(replace)
@@ -123,6 +145,16 @@ ConflictWindow::MessageReceived(BMessage* msg)
 
 
 BStringView*
+ConflictWindow::_CreateLabelView(const char* label)
+{
+	BString text(label);
+	text += ':';
+
+	return new BStringView("", text);
+}
+
+
+BStringView*
 ConflictWindow::_CreateAttrView(const char* file, const char* folder)
 {
 	BDirectory dir(folder);
@@ -141,11 +173,14 @@ ConflictWindow::_CreateAttrView(const char* file, const char* folder)
 
 	char size[128];
 	string_for_size(attr.st_size, size, sizeof(size));
+	str << "; " << size;
 
-	BString text("[ ");
-	text << str << "; " << size << " ]";
-	BStringView* view = new BStringView("", text);
-	view->SetAlignment(B_ALIGN_CENTER);
+	BStringView* view = new BStringView("", str);
+
+	BFont font;
+	view->GetFont(&font);
+	font.SetFace(B_ITALIC_FACE);
+	view->SetFont(&font);
 
 	return view;
 }
