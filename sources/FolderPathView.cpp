0837dd864cca8c7cd65df74a3af1cbaf8c0e5890
Humdinger <humdingerb@gmail.com>
2017-08-12 19:11:17 +0200

    Conflict window: Improved opening folder of an file
    Emulating Web+'s DownloadProgressView.cpp:
    http://xref.plausible.coop/source/xref/haiku/src/apps/webpositive/DownloadProgressView.cpp#532
    
    Crowded folders may still being populated after selecting the file, scrolling
    it out of sight. We therefore wait 0.2 sec until sending the select-message
    again.

--- sources/FolderPathView.cpp
+++ sources/FolderPathView.cpp
@@ -14,6 +14,7 @@
 #include <Catalog.h>
 #include <Cursor.h>
 #include <MenuItem.h>
+#include <NodeInfo.h>
 #include <Path.h>
 #include <Roster.h>
 #include <Window.h>
@@ -118,43 +119,30 @@ FolderPathView::MouseUp(BPoint where)
 void
 FolderPathView::_OpenFolder()
 {
-	be_roster->Launch(&fFolderRef);
+	BEntry selected(&fFileRef);
+	if (!selected.Exists())
+		return;
 
-	BMessage cnt(B_COUNT_PROPERTIES);
-	cnt.AddSpecifier("Selection");
-	cnt.AddSpecifier("Poses");
-	cnt.AddSpecifier("Window", fFolderRef.name);
+	// Ask Tracker to open the containing folder and select the
+	// file inside it.
+	BMessenger trackerMessenger("application/x-vnd.Be-TRAK");
 
-	BPath path(&fFolderRef);
-	BMessage cnt2(B_COUNT_PROPERTIES);
-	cnt2.AddSpecifier("Selection");
-	cnt2.AddSpecifier("Poses");
-	cnt2.AddSpecifier("Window", path.Path());
+	if (trackerMessenger.IsValid()) {
+		BMessage selectionCommand(B_REFS_RECEIVED);
+		status_t status = selectionCommand.AddRef("refs", &fFolderRef);
 
-	int32 count = 0;
-	int32 count2 = 0;
-	int32 temp = 0;
-	BMessage reply;
-	BMessage reply2;
-	BMessenger msgr("application/x-vnd.Be-TRAK");
-	do {
-		BMessage sel('Tsel');
-		sel.AddRef("refs", &fFileRef);
-		msgr.SendMessage(&sel);
+		node_ref selectedRef;
+		if (selected.GetNodeRef(&selectedRef) == B_OK) {
+			selectionCommand.AddData("nodeRefToSelect", B_RAW_TYPE,
+				(void*)&selectedRef, sizeof(node_ref));
+		}
+		trackerMessenger.SendMessage(&selectionCommand);
 
-		snooze(100000);	// wait 0.1 sec
-
-		msgr.SendMessage(&cnt, &reply);
-		reply.FindInt32("result", &count);
-
-		msgr.SendMessage(&cnt2, &reply2);
-		reply2.FindInt32("result", &count2);
-	} while (count == 0 && count2 == 0);
-
-	snooze(200000);	// wait 0.2 sec for large folders to populate
-	BMessage sel('Tsel');
-	sel.AddRef("refs", &fFileRef);
-	msgr.SendMessage(&sel);
+		snooze(200000);	// wait 0.2 sec for large folders to populate
+		BMessage select('Tsel');
+		select.AddRef("refs", &fFileRef);
+		trackerMessenger.SendMessage(&select);
+	}
 }
 
 
